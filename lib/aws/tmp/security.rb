fd74c28890f954c315a533e23590aae5e7eaf7e6
Stewart McKee <stewart@theizone.co.uk>
2016-01-25 11:37:31 +0000

    initial commit

---
+++ lib/aws/tmp/security.rb
@@ -0,0 +1,9 @@
+require "aws/tmp/security/version"
+
+module Aws
+  module Tmp
+    module Security
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/aws/tmp/security/version.rb
@@ -0,0 +1,7 @@
+module Aws
+  module Tmp
+    module Security
+      VERSION = "0.1.0"
+    end
+  end
+end
