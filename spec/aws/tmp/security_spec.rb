1fb2440a7db14722f766cadececc7e6dbf698d1d
Stewart McKee <stewart@theizone.co.uk>
2016-01-25 11:43:23 +0000

    adding pending specs

--- spec/aws/tmp/security_spec.rb
+++ spec/aws/tmp/security_spec.rb
@@ -5,4 +5,16 @@ describe Aws::Tmp::Security do
     expect(Aws::Tmp::Security::VERSION).not_to be nil
   end
 
+  it 'should load configured credentials for aws'
+  it 'should load configured groups to update'
+  it 'should locate current external ip'
+  it 'should add external ip to relevant groups'
+  it 'should store current external ip'
+  it 'should clear stale addresses from groups'
+  it 'should run as daemon to check external ip and automatcially add and remove'
+
+  it 'should react to network events to update security groups'
+
+  
+
 end
